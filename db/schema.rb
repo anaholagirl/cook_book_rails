02bf20406f0a87d0523e15ed4898b1d3c442f6e5
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-09-04 16:05:16 -0700

    Add tag class to the database and validate presence ofname.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904170816) do
+ActiveRecord::Schema.define(version: 20140904230221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20140904170816) do
     t.string "author"
     t.text   "ingredients"
     t.text   "instructions"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string "name"
   end
 
 end

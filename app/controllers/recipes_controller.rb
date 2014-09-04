ecef0eda4949e6ee24906fd6bb47b1b694193a8e
Linda Gibson & Marina Neumann <student@epicodus.com>
2014-09-04 15:57:31 -0700

    Introduced bootstrap stylings, implemented correct routes from one feature to another, and established fully working recipe aspect of our glorious cook book.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,7 +18,7 @@ class RecipesController <ApplicationController
 
     if @recipe.save
       flash[:notice]= "Your recipe was added to the Joy of Cooking!!"
-      redirect_to('recipes/success.html.erb')
+      render('recipes/success.html.erb')
     else
       render('recipes/new.html.erb')
     end
@@ -27,5 +27,25 @@ class RecipesController <ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     render('recipes/show.html.erb')
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    render('recipes/destroy.html.erb')
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+    render('recipes/edit.html.erb')
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(params[:recipe])
+      render('recipes/success.html.erb')
+    else
+      render('recipes/edit.html.erb')
+    end
   end
 end
